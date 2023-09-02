99f16385aa5ae1e3b0f3e77c5be5a491e1f3fe5c
liximi <973695015@qq.com>
2023-09-03 00:08:53 +0800

    udpate

--- scripts/widgets/agcw_hud.lua
+++ scripts/widgets/agcw_hud.lua
@@ -17,24 +17,24 @@ local HUD = Class(Widget, function(self, owner)
 	self.interactive_functions = self:AddChild(Interactive(owner))
 
 	--敌人刷新按钮
-	self.current_turn_text = self.top_root:AddChild(Text(UIFONT, 28, tostring(TheWorld.net.replica.agcw_enemy_spawner:GetCurrentTurn())))
-	self.current_turn_text:SetPosition(0, -50)
-	self.start_next_turn_bt = self.top_root:AddChild(Templates.StandardButton(function () self:StartNextTurn() end,
-		STRINGS.UI.SC_NEXT_TURN, {200, 50}))
-	self.start_next_turn_bt:SetPosition(0, -90)
+	-- self.current_turn_text = self.top_root:AddChild(Text(UIFONT, 28, tostring(TheWorld.net.replica.agcw_enemy_spawner:GetCurrentTurn())))
+	-- self.current_turn_text:SetPosition(0, -50)
+	-- self.start_next_turn_bt = self.top_root:AddChild(Templates.StandardButton(function () self:StartNextTurn() end,
+	-- 	STRINGS.UI.SC_NEXT_TURN, {200, 50}))
+	-- self.start_next_turn_bt:SetPosition(0, -90)
 
-	self.inst:ListenForEvent("agcw_enemy_spawner.current_turn", function()
-		local current_turn = TheWorld.net.replica.agcw_enemy_spawner:GetCurrentTurn()
-		self.current_turn_text:SetString(tostring(current_turn))
-	end, TheWorld.net)
+	-- self.inst:ListenForEvent("agcw_enemy_spawner.current_turn", function()
+	-- 	local current_turn = TheWorld.net.replica.agcw_enemy_spawner:GetCurrentTurn()
+	-- 	self.current_turn_text:SetString(tostring(current_turn))
+	-- end, TheWorld.net)
 
-	self.inst:ListenForEvent("agcw_enemy_spawner.is_started", function()
-		if TheWorld.net.replica.agcw_enemy_spawner:IsStarted() then
-			self.start_next_turn_bt:Disable()
-		else
-			self.start_next_turn_bt:Enable()
-		end
-	end, TheWorld.net)
+	-- self.inst:ListenForEvent("agcw_enemy_spawner.is_started", function()
+	-- 	if TheWorld.net.replica.agcw_enemy_spawner:IsStarted() then
+	-- 		self.start_next_turn_bt:Disable()
+	-- 	else
+	-- 		self.start_next_turn_bt:Enable()
+	-- 	end
+	-- end, TheWorld.net)
 end)
 
 
