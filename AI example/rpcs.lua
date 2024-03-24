c479f05389061495c89053b9a6c23338eacf5b47
liximi <973695015@qq.com>
2024-03-25 01:05:23 +0800

    删除文件

--- AI example/rpcs.lua
+++
@@ -1,7 +0,0 @@
---[[On Server]]
-
-AddModRPCHandler(AGCW.RPC_NAMESPACE, "start_next_turn", function(player)
-	if TheWorld.net.components.agcw_enemy_spawner then
-		TheWorld.net.components.agcw_enemy_spawner:Start()
-	end
-end)
