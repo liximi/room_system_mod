c57c5398e7f914656530927f1c85098581ffae88
liximi <973695015@qq.com>
2024-09-17 21:28:19 +0800

    新增工坊介绍页面文本，英文翻译

---
+++ scripts/localization/english_ui.lua
@@ -0,0 +1,13 @@
+STRINGS.TILE_SELECTION_HOVERER_TEXT_SELECT = "点击或拖拽框选地块"
+STRINGS.TILE_SELECTION_HOVERER_TEXT_DISELECT = "点击或拖拽取消选择"
+STRINGS.M23M_UI = {
+	ROOM = "Room",
+	ROOM_OVERVIEW = "Rooms Overview",
+	SIZE_LIMITATION = "Size requirements:",
+	SIZE_LIMITATION2 = "1 unit equals 1/16 of the tile",
+	MUST_ITEMS_TITLE = "The following buildings/items need to be placed in the room:",
+	MUST_TILES_TITLE = "The room can only contain the following tiles:",
+	ANY = "One of:",
+	NONE = "Nothing",
+	DRAG = "Drag to move",
+}
