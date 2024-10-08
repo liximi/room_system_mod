25c1c22e89369791362d9b6181ba9d52d201238b
liximi <liangxiangming@aquilatech.ai>
2024-10-08 18:25:32 +0800

    hoverer的兼容性优化

--- m23m_main_scripts/ui.lua
+++ m23m_main_scripts/ui.lua
@@ -12,117 +12,33 @@ end)
 
 local HUD = require "widgets/m23m_hud"
 AddClassPostConstruct("widgets/controls", function(self)
-    --添加新UI
-    self.m23m_hud = self:AddChild(HUD(self.owner))
-    self.m23m_hud:MoveToFront()
+	--添加新UI
+	self.m23m_hud = self:AddChild(HUD(self.owner))
+	self.m23m_hud:MoveToFront()
 
 	local mod_version = env.modinfo.version
 	if mod_version == "dev" then
-		self.xp = self:AddChild(Text(UIFONT, 22, "X正"))
-		self.xn = self:AddChild(Text(UIFONT, 22, "X负"))
-		self.zp = self:AddChild(Text(UIFONT, 22, "Z正"))
-		self.zn = self:AddChild(Text(UIFONT, 22, "Z负"))
+		self.xp = self:AddChild(Text(UIFONT, 16, "x正"))
+		self.xn = self:AddChild(Text(UIFONT, 16, "x负"))
+		self.zp = self:AddChild(Text(UIFONT, 16, "z正"))
+		self.zn = self:AddChild(Text(UIFONT, 16, "z负"))
 
-		self.inst:DoPeriodicTask(FRAMES, function ()
+		self.inst:DoPeriodicTask(FRAMES, function()
 			local pos = ThePlayer:GetPosition()
-			self.xp:SetPosition(TheSim:GetScreenPos((pos+Vector3(2,0,0)):Get()))
-			self.xn:SetPosition(TheSim:GetScreenPos((pos+Vector3(-2,0,0)):Get()))
-			self.zp:SetPosition(TheSim:GetScreenPos((pos+Vector3(0,0,2)):Get()))
-			self.zn:SetPosition(TheSim:GetScreenPos((pos+Vector3(0,0,-2)):Get()))
+			self.xp:SetPosition(TheSim:GetScreenPos((pos + Vector3(1.2, 0, 0)):Get()))
+			self.xn:SetPosition(TheSim:GetScreenPos((pos + Vector3(-1.2, 0, 0)):Get()))
+			self.zp:SetPosition(TheSim:GetScreenPos((pos + Vector3(0, 0, 1.2)):Get()))
+			self.zn:SetPosition(TheSim:GetScreenPos((pos + Vector3(0, 0, -1.2)):Get()))
 		end)
 	end
 end)
 
 
--- require "widgets.hoverer"
--- 向鼠标悬浮文本中加入当前房间的信息, 直接复制了原本的OnUpdate进行修改
-local SHOW_DELAY = 0
-AddClassPostConstruct("widgets/hoverer",function(self)
-    self.OnUpdate = function (self)
-		if self.owner.components.playercontroller == nil or not self.owner.components.playercontroller:UsingMouse() then
-			if self.shown then
-				self:Hide()
-			end
-			return
-		elseif not self.shown then
-			if not self.forcehide then
-				self:Show()
-			else
-				return
-			end
-		end
-
-		local str = nil
-		local colour = nil
-		if not self.isFE then
-			str = self.owner.HUD.controls:GetTooltip() or self.owner.components.playercontroller:GetHoverTextOverride()
-			self.text:SetPosition(self.owner.HUD.controls:GetTooltipPos() or self.default_text_pos)
-			if self.owner.HUD.controls:GetTooltip() ~= nil then
-				colour = self.owner.HUD.controls:GetTooltipColour()
-			end
-		else
-			str = self.owner:GetTooltip()
-			self.text:SetPosition(self.owner:GetTooltipPos() or self.default_text_pos)
-		end
-
-		local secondarystr = nil
-		local lmb = nil
-		if str == nil and not self.isFE and self.owner:IsActionsVisible() then
-			lmb = self.owner.components.playercontroller:GetLeftMouseAction()
-			if lmb ~= nil then
-				local overriden
-				str, overriden = lmb:GetActionString()
-
-				if lmb.action.show_primary_input_left then
-					str = TheInput:GetLocalizedControl(TheInput:GetControllerID(), CONTROL_PRIMARY) .. " " .. str
-				end
-
-				if colour == nil then
-					if lmb.target ~= nil then
-						if lmb.invobject ~= nil and not (lmb.invobject:HasTag("weapon") or lmb.invobject:HasTag("tool")) then
-							colour = lmb.invobject:GetIsWet() and WET_TEXT_COLOUR or NORMAL_TEXT_COLOUR
-						else
-							colour = lmb.target:GetIsWet() and WET_TEXT_COLOUR or NORMAL_TEXT_COLOUR
-						end
-					elseif lmb.invobject ~= nil then
-						colour = lmb.invobject:GetIsWet() and WET_TEXT_COLOUR or NORMAL_TEXT_COLOUR
-					end
-				end
-
-				if not overriden and lmb.target ~= nil and lmb.invobject == nil and lmb.target ~= lmb.doer then
-					local name = lmb.target:GetDisplayName()
-					if name ~= nil then
-						local adjective = lmb.target:GetAdjective()
-						str = str.." "..(adjective ~= nil and (adjective.." "..name) or name)
-
-						if lmb.target.replica.stackable ~= nil and lmb.target.replica.stackable:IsStack() then
-							str = str.." x"..tostring(lmb.target.replica.stackable:StackSize())
-						end
-
-						--NOTE: This won't work on clients. Leaving it here anyway.
-						if lmb.target.components.inspectable ~= nil and lmb.target.components.inspectable.recordview and lmb.target.prefab ~= nil then
-							ProfileStatsSet(lmb.target.prefab.."_seen", true)
-						end
-					end
-				end
-			end
-			local aoetargeting = self.owner.components.playercontroller:IsAOETargeting()
-			local rmb = self.owner.components.playercontroller:GetRightMouseAction()
-			if rmb ~= nil then
-				if rmb.action.show_secondary_input_right then
-					secondarystr = rmb:GetActionString() .. " " .. TheInput:GetLocalizedControl(TheInput:GetControllerID(), CONTROL_SECONDARY)
-				elseif rmb.action ~= ACTIONS.CASTAOE then
-					secondarystr = TheInput:GetLocalizedControl(TheInput:GetControllerID(), CONTROL_SECONDARY)..": "..rmb:GetActionString()
-				elseif aoetargeting and str == nil then
-					str = rmb:GetActionString()
-				end
-			end
-			if aoetargeting and secondarystr == nil then
-				secondarystr = TheInput:GetLocalizedControl(TheInput:GetControllerID(), CONTROL_SECONDARY)..": "..STRINGS.UI.HUD.CANCEL
-			end
-		end
-
-		---- MOD
+AddClassPostConstruct("widgets/hoverer", function(self)
+	local old_OnUpdate = self.OnUpdate
+	function self:OnUpdate()
+		old_OnUpdate(self)
+		local str = self.str
 		local is_on_ui = TheInput:GetHUDEntityUnderMouse()
 		if ThePlayer.is_room_view_active and not is_on_ui then
 			local mouse_pos = TheInput:GetWorldPosition()
@@ -138,46 +54,28 @@ AddClassPostConstruct("widgets/hoverer",function(self)
 				end
 				if room_name then
 					if str then
-						str = str .. "\n" .. STRINGS.M23M_UI.ROOM ..": ".. room_name
+						str = str .. "\n" .. STRINGS.M23M_UI.ROOM .. ": " .. room_name
 					else
-						str = "\n" .. STRINGS.M23M_UI.ROOM ..": ".. room_name
+						str = STRINGS.M23M_UI.ROOM .. ": " .. room_name
 					end
 				end
 			end
 		end
-		----
 
 		if str == nil then
 			self.text:Hide()
-		elseif self.str ~= self.lastStr then
-			self.lastStr = self.str
-			self.strFrames = SHOW_DELAY
 		else
-			self.strFrames = self.strFrames - 1
 			if self.strFrames <= 0 then
-				if lmb ~= nil and lmb.target ~= nil and lmb.target:HasTag("player") then
-					self.text:SetColour(unpack(lmb.target.playercolour))
-				else
-					self.text:SetColour(unpack(colour or NORMAL_TEXT_COLOUR))
-				end
 				self.text:SetString(str)
 				self.text:Show()
 			end
 		end
 
-		if secondarystr ~= nil then
-			self.secondarytext:SetString(secondarystr)
-			self.secondarytext:Show()
-		else
-			self.secondarytext:Hide()
-		end
-
-		local changed = self.str ~= str or self.secondarystr ~= secondarystr
+		local changed = self.str ~= str
 		self.str = str
-		self.secondarystr = secondarystr
 		if changed then
 			local pos = TheInput:GetScreenPosition()
 			self:UpdatePosition(pos.x, pos.y)
 		end
-    end
+	end
 end)
