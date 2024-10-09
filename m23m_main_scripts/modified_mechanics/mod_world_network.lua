233d1f63f1353bf3db52b91e34db57d38e21b77e
liximi <liangxiangming@aquilatech.ai>
2024-10-09 16:31:16 +0800

    update

--- m23m_main_scripts/modified_mechanics/mod_world_network.lua
+++ m23m_main_scripts/modified_mechanics/mod_world_network.lua
@@ -13,19 +13,6 @@ local function PostInit(inst)
         inst.M23M_RegionMgr = inst:AddComponent("m23m_region_manager")
         local map_w, map_h = TheWorld.Map:GetSize()
         inst.M23M_RegionMgr:Generation(map_w * 4, map_h * 4, 34, 34)    --因为墙体每个只占1格，所以整个地图的尺寸必须是这么多，section尺寸过小会导致性能问题
-        -- local waters = {}
-        -- local min_x, min_z = inst.M23M_RegionMgr:GetPointAtTileCoords(0, 0)
-        -- local max_x, max_z = inst.M23M_RegionMgr:GetPointAtTileCoords(map_w * 4, map_h * 4)
-        -- for  i = min_x, max_x do
-        --     for j = min_z, max_z do
-        --         local is_water = TheWorld.Map:IsOceanTileAtPoint(i, 0, j)
-        --         if is_water then
-        --             table.insert(waters, {inst.M23M_RegionMgr:GetTileCoordsAtPoint(i, j)})
-        --         end
-        --     end
-        -- end
-        -- inst.M23M_RegionMgr:AddWaters(waters)
-        -- waters = nil
         collectgarbage("collect")
         local end_clock = os:clock()
         local memory_after = collectgarbage("count")
